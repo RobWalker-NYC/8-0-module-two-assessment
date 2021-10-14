a4ae655ff9222050c21e9d3dc08e5973d39fd387
rob <robwalker@pursuit.org>
2021-10-14 18:09:00 -0400

    pull down list now works

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 fetch("https://ghibliapi.herokuapp.com/films")
 .then((res) => {
     return res.json();
-    console.log("title");
 
 })
 .then((movies) => {
     console.log(movies);
 
-// let dropDown = document.querySelector("#dropdown")
-// for(let movie of movies)
-//      let option = document.createElement("option");
-//      option.setAttribute("value", movie.title);
-//      option.textContent = movie.title;
-//     //  option.value = movie.title;
-//      dropDown.append(option);
+let dropDown = document.querySelector("#dropdown")
 
-// dropDown.addEventListener("change", (e) => {
+for(let movie of movies) {
+     let option = document.createElement("option");
+     option.setAttribute("value", movie.title);
+     option.textContent = movie.title;
+     dropDown.append(option);
+}
 
-// });
+let title = document.querySelector("title");
+const year = document.querySelector("year");
+const description = document.querySelector("description");
 
+let selectedMovie
+
+dropDown.addEventListener("change", () => {
+    for(let movie of movies) {
+    if(dropDown.value === movie.title) {
+        selectedMovie = movie;
+        console.log(movie);
+        year.textContent = movie.year;
+        description.textContent = movie.description;
+    }
+  }
+});
 
 
 }).catch((err) => {
