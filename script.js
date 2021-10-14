9332c630aabaabc95af6d4732b6911ce22187b38
rob <robwalker@pursuit.org>
2021-10-14 16:44:44 -0400

    still working on api

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-fetch("http://https://ghibliapi.herokuapp.com/")
-.then((res))
+fetch("https://ghibliapi.herokuapp.com/films")
+.then((res) => {
+    return res.json();
+    console.log("title");
+
+})
+.then((movies) => {
+    console.log(movies);
+
+// let dropdown = document.querySelector('#dropdown')
+// for(let movie of movies)
+//      let option = document.createElement("option");
+//     //  option.setAttribute("value", movie.title);
+//      option.textContent = movie.title;
+//      option.value = movie.title;
+//      dropdown.append(option);
+
+// dropdown.addEventListener("change", (e) => {
+
+// });
+
+
+
+}).catch((err) => {
+console.log(err);
+});
